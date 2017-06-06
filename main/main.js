30d129097e01d7c1ddd0fcf98500eae967b40699
geekersnow <geekersnow@users.noreply.github.com>
2017-06-06 23:13:47 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,18 +1,31 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+    console.log(`o) 最小值 = ${sequence.minimum()}\n
+o) 最大值 = ${sequence.maximum()}\n
+o) 元素数量 = ${sequence.elenumber()}\n
+o) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+	this.array = input;
   }
 
   minimum() {
-    // Write your code here
+	return Math.min.apply(null, this.array);
   }
-
-  // Write your code here
+  maximum(){
+	return Math.max.apply(null, this.array);
+  }
+  elenumber(){
+	return this.array.length;  
+  }
+  average(){
+	var sum = 0, result = 0, average = 0;
+	for ( var i = 0; i< this.array.length; i++ ) 
+		sum += this.array[i];	
+	result = sum / this.array.length; 
+	return parseFloat(result.toFixed(2)); 
+	
+  }
 }
